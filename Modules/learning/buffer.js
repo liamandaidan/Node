5e58644bdd45ed2055e3b15a7fe5daf034fbe103
Liam <liammclaughlinis@gmail.com>
2022-08-14 00:10:54 -0600

    File IO

---
+++ Modules/learning/buffer.js
@@ -0,0 +1,6 @@
+const buffer1 = Buffer.from('hello'); // Output: [104, 101, 108, 108, 111]
+const buffer2 = Buffer.from('world'); // Output:[119, 111, 114, 108, 100]
+const array = [buffer1, buffer2];
+const bufferConcat = Buffer.concat(array);
+ 
+console.log(bufferConcat.toString()); // Output: [104, 101, 108, 108, 111, 119, 111, 114, 108, 100]
